3639c1d022d3cd91aa3e16b5181f930c67d3680f
Sand22 <sandarous222@gmail.com>
2014-09-20 18:07:04 +0200

    default files

---
+++ public/modules/auth/tests/auth/controllers/UserCtrlSpec.js
@@ -0,0 +1,13 @@
+describe('UserCtrl', function() {
+
+    'use strict';
+    
+    describe('ExampleTestSuite', function() {
+        
+        it('example test should pass', function () {
+            expect(1).toBe(1);
+        });
+        
+    });
+    
+});
