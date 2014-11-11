20f83e188bc229d5f16e28ae18ea3db06d22ae04
Sand22 <sandarous222@gmail.com>
2014-11-11 13:06:48 +0100

    moved app config to sepparate file

--- public/app.js
+++ public/app.js
@@ -1,18 +1,1 @@
-angular.module('taskMate', ['ngRoute'])
-  .config(function ($routeProvider, $locationProvider) {
-    'use strict';
-
-    $routeProvider
-      .when('/', {
-        templateUrl: '/views/common/partials/main'
-      })
-      .when('/404', {
-        templateUrl: '/views/common/partials/404'
-      })
-      .otherwise({
-        redirectTo: '/404'
-      });
-
-    $locationProvider.html5Mode(true);
-
-  });
+angular.module('app', ['ngRoute']);
---
+++ public/config.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+    .config(config);
+
+  /* @ngInject */
+  function config($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '/views/common/partials/main'
+      })
+      .when('/404', {
+        templateUrl: '/views/common/partials/404'
+      })
+      .otherwise({
+        redirectTo: '/404'
+      });
+
+    $locationProvider.html5Mode(true);
+  }
+})();
