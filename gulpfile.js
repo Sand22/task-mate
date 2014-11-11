fceccfed8a9d51e28ccbf477f00fe499bf405e57
Sand22 <sandarous222@gmail.com>
2014-11-11 14:02:23 +0100

    added gulp annotate for automate dependency injection

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,69 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    karma = require('gulp-karma'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    prefix = require('gulp-autoprefixer'),
-    compressor = require('gulp-compressor'),
-    jshint = require('gulp-jshint'),
-    stylish = require('jshint-stylish'),
-    rename = require('gulp-rename'),
-    testFiles = 'public/modules/**/tests/**/**/*.js',
-    scripts = [
-        'vendor/**/*.min.js',
-        'public/app.js',
-        'public/modules/**/**/*.js',
-        '!public/modules/**/tests/**/**/*.js'
-    ],
-    serverSideScripts = [
-        'server/server.js',
-        'server/**/*.js'
-    ],
-    sassFiles = 'public/assets/stylesheets/*.scss';
+  sass = require('gulp-sass'),
+  karma = require('gulp-karma'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  prefix = require('gulp-autoprefixer'),
+  compressor = require('gulp-compressor'),
+  jshint = require('gulp-jshint'),
+  stylish = require('jshint-stylish'),
+  rename = require('gulp-rename'),
+  ngAnnotate = require('gulp-ng-annotate'),
+  testFiles = 'public/**/tests/**/**/*.js',
+  scripts = [
+    'public/vendor/!(bootstrap|jquery|angular-mocks)/*.min.js',
+    'public/*.js',
+    'public/!(vendor)/**/*.js',
+    '!public/**/tests/**/**/*.js'
+  ],
+  serverSideScripts = [
+    'server.js',
+    'server/**/*.js'
+  ],
+  sassFiles = 'public/assets/stylesheets/*.scss';
 
 gulp.task('sass', function () {
-    'use strict';
+  'use strict';
 
-    gulp.src(sassFiles)
-        .pipe(sass({
-            sourceMap: 'sass',
-            sourceComments: 'map'
-        }))
-        .pipe(prefix('last 1 version', '> 1%', 'ie 8', 'ie 7'))
-        .pipe(compressor())
-        .pipe(rename('app.min.css'))
-        .pipe(gulp.dest('public/assets/stylesheets/build'));
+  gulp.src(sassFiles)
+    .pipe(sass({
+      sourceMap: 'sass',
+      sourceComments: 'map'
+    }))
+    .pipe(prefix('last 1 version', '> 1%', 'ie 8', 'ie 7'))
+    .pipe(compressor())
+    .pipe(rename('app.min.css'))
+    .pipe(gulp.dest('public/assets/stylesheets/build'));
 });
 
-gulp.task('test', function() {
-    'use strict';
+gulp.task('test', function () {
+  'use strict';
 
-    gulp.src(testFiles)
-        .pipe(karma({
-            configFile: 'karma.conf.js',
-            action: 'run'
-        }));
+  gulp.src(testFiles)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }));
 });
 
-gulp.task('uglify', function() {
-    'use strict';
+gulp.task('uglify', function () {
+  'use strict';
 
-    gulp.src(scripts)
-        .pipe(uglify())
-        .pipe(concat('app.min.js'))
-        .pipe(gulp.dest('public'));
+  gulp.src(scripts)
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('public'));
 });
 
 gulp.task('jshint', function () {
-    'use strict';
+  'use strict';
 
-    return gulp.src(scripts.slice(1,3).concat(serverSideScripts))
-        .pipe(jshint())
-        .pipe(jshint.reporter(stylish));
+  return gulp.src(scripts.slice(1, 3).concat(serverSideScripts))
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('annotate', function () {
+  'use strict';
 });
